e5dfcda77b121374b6d91410043e686c30b903ed
Vegas2101 <matusov.210121@gmail.com>
2025-02-14 19:15:45 +0300

    Example 2

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return " Кошей умер \uD83D\uDC80";
+    }
 }
